c2dd56e47d6bffd19886ef18c1832f6e26f9a147
Bin Wang <bin.wang@mail.binwang.me>
2026-03-24 19:20:53 -0400

    update content type and UA

--- src/index.ts
+++ src/index.ts
@@ -36,8 +36,7 @@ export default {
 
         const init = {
             headers: {
-                "content-type": "text/html;charset=UTF-8",
-                "User-Agent": "Cloudflare worker http proxy",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
             },
         };
 
